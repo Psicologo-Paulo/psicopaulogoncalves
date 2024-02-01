27c0cc4d0b20486a9724ad7b492c5e93336a41b9
Psicologo-Paulo <paulinhoag@hotmail.com>
2024-02-01 17:32:24 -0300

    add gtag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,22 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { usePathname, useSearchParams } from "next/navigation";
+import Script from "next/script";
 
 export default function Document() {
+
+
   return (
     <Html className="scroll-smooth" lang="pt-br">
-      <Head title="Piscologo Paulo" />
+      <Head title="Piscologo Paulo">
+        <Script async strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.ADS}`}>
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', ${process.env.ADS});`}
+        </Script>
+        
+      </Head>
       <body>
         <Main />
         <NextScript />
